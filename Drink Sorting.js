aa62a7eed0dea6e4a1b47e1167f664e84ecd756c
cchow33 <109078860+cchow33@users.noreply.github.com>
2022-08-08 23:49:38 -0400

    Update Drink Sorting.js

--- Drink Sorting.js
+++ Drink Sorting.js
@@ -1,3 +1,7 @@
+// You will be given an array of drinks, with each drink being an object with two properties: name and price. 
+// Create a function that has the drinks array as an argument and return the drinks objects sorted by price in ascending order.
+
+
 function sortDrinkByPrice(drinks) {
 	return drinks.sort((a,b) => a.price - b.price)
 }
